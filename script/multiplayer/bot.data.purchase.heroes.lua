45e45b646aef3c25507a6fbe632ed10e86482fbe
Xpycmb8 <cool.wertex@yandex.ru>
2025-01-19 02:36:45 +0300

    G2

---
+++ script/multiplayer/bot.data.purchase.heroes.lua
@@ -0,0 +1,116 @@
+Purchases["heroes"] = {
+	{--purchase 1
+		Repeat = 2,
+		Units = {
+			["ger"] = {--Germany
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad16(ger)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad17(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad18(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad19(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad20(ger)"},
+			},
+			["rus"] = {--Russia
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad16(rus)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad17(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad18(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad19(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad20(rus)"},
+			}
+		}
+	},
+	{--purchase 2
+		Repeat = 1,
+		Units = {
+			["ger"] = {--Germany
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad16(ger)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad17(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad18(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad19(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad20(ger)"},
+			},
+			["rus"] = {--Russia
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad16(rus)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad17(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad18(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad19(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad20(rus)"},
+			}
+		}
+	},
+	{--purchase 3
+		Repeat = 1,
+		Units = {
+			["ger"] = {--Germany
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad16(ger)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad17(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad18(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad19(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad20(ger)"},
+			},
+			["rus"] = {--Russia
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad16(rus)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad17(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad18(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad19(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad20(rus)"},
+			}
+		}
+	},
+	{--purchase 4
+		Repeat = 1,
+		Units = {
+			["ger"] = {--Germany
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad16(ger)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad17(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad18(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad19(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad20(ger)"},
+			},
+			["rus"] = {--Russia
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad16(rus)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad17(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad18(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad19(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad20(rus)"},
+			}
+		}
+	},
+	{--purchase 5
+		Repeat = 3, --infinite
+		Units = {
+			["ger"] = {--Germany
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad16(ger)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad17(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad18(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad19(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad20(ger)"},
+			},
+			["rus"] = {--Russia
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad16(rus)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad17(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad18(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad19(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad20(rus)"},
+			}
+		}
+	},
+	{--purchase 6
+		Repeat = 0, --infinite
+		Units = {
+			["ger"] = {--Germany
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad16(ger)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad17(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad18(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad19(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad20(ger)"},
+			},
+			["rus"] = {--Russia
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad16(rus)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad17(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad18(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad19(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad20(rus)"},
+			}
+		}
+	}
+}
